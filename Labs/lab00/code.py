229139d1361050eebb087a84d03ff7f28d3084b8
ilovecpprospyjavac <1589382624@qq.com>
2024-08-18 17:18:19 +0800

    对Labs/lab00/code.py的第一次修改

---
+++ Labs/lab00/code.py
@@ -0,0 +1,13 @@
+def fizzbuzz(num):
+    if num%3==0 and num%5!=0:
+        print("num: fizz")
+    elif num%5==0 and num%3!=0:
+        print("num: buzz")
+    elif num%3==0 and num%5==0:
+        print("num: fizzbuzz")
+    else:
+        return
+num = input()
+num = int(num)
+fizzbuzz(num)
+
